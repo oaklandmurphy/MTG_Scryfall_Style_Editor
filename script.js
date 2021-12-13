221d3be5cf5adbbb1c905c367c8e9c70d10ceab2
oaklandmurphy <aidan@niallmurphy.net>
2021-12-12 18:37:55 -0800

    Add files via upload
    added dual faced card functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var endpoint = "https://api.scryfall.com/"
 
+// sets all displays to the default setting
 function display_default() {
 	// print the current guess for the card being searched
 	$("#card_title").text("No Card Searched");
@@ -12,56 +13,82 @@ function display_default() {
 
 }
 
-// set all feilds to default values on page access
-display_default()
-
-// activated when the search bar is edited
+// searches the Scryfall API .5 seconds after the user finishes typing
 $(document).ready(function(){
-    $("#card_name_search").on("input", function(){
+	$('#card_name_search').bind('input keyup', function(){
+	    var $this = $(this);
+	    var delay = 500; // 2 seconds delay after last input
 
-        // store the current input in the bar
-        var card_searched = $(this).val()
-        console.log(card_searched)
+	    // handle debouncing
+	    clearTimeout($this.data('timer'));
+	    $this.data('timer', setTimeout(function(){
+	        $this.removeData('timer');
 
-        // if the serch is too short to match any cards show the default
-        if(card_searched.length < 2) {	
 
-			display_default()
+	        // store the current input in the bar
+	        var card_searched = document.getElementById('card_name_search').value
+	        console.log(card_searched)
 
-		//otherwise search
-        }else {
+	        // if the serch is too short to match any cards show the default
+	        if(card_searched.length < 2) {	
 
-	        // try to access the scryfall api for matching cards
-	        $.getJSON("https://api.scryfall.com/cards/autocomplete?q=" + card_searched, function(choices){
-				
-				// if there are no matches show the default
-	        	if(choices.data.length == 0) {
-	        		display_default()
+				display_default()
 
-	        	// otherwise grab the best match's data
-	        	}else {
+			//otherwise search
+	        }else {
 
-		        	var best_match = choices.data[0]
+		        // try to access the scryfall api for matching cards
+		        $.getJSON("https://api.scryfall.com/cards/autocomplete?q=" + card_searched, function(choices){
+					
+					// if there are no matches show the default
+		        	if(choices.data.length == 0) {
+		        		display_default()
 
-		        	console.log(choices)
+		        	// otherwise grab the best match's data
+		        	}else {
 
-		        	$.getJSON("https://api.scryfall.com/cards/named?fuzzy=" + best_match, function(data){
-						// store relevant card data
-						var card_title = data.name
-						var card_oracle_text = data.oracle_text
-						var card_img_front = data.image_uris.large
+			        	var best_match = choices.data[0]
 
-						// print the current guess for the card being searched
-						$("#card_title").text(card_title);
+			        	console.log(choices)
 
-						// print the current guess for the oracle text of the card being searched
-						$("#card_oracle_text").text(card_oracle_text);
+			        	$.getJSON("https://api.scryfall.com/cards/named?fuzzy=" + best_match, function(data){
 
-						//show the image of the current card being searched
-						$('#card_img_front').attr('src', card_img_front);
-					});
-	        	}
-			});
-    	}
-    });
+			        		console.log(data)
+							
+							// store relevant card data
+							var card_title = data.name
+							var card_oracle_text
+							var card_img_front
+
+							//check for strange layouts
+							if(typeof(data.card_faces) != "undefined") {
+								card_oracle_text = data.card_faces[0].oracle_text + "\r\n//\r\n" + data.card_faces[1].oracle_text
+
+								//deal with transform cards
+								if(data.layout == "transform" || data.layout == "modal_dfc" || data.layout == "flip") {
+									card_img_front = data.card_faces[0].image_uris.large
+								}else {
+									card_img_front = data.image_uris.large
+								}
+							}else {
+								card_oracle_text = data.oracle_text
+								card_img_front = data.image_uris.large
+							}
+
+							
+
+							// print the current guess for the card being searched
+							$("#card_title").text(card_title);
+
+							// print the current guess for the oracle text of the card being searched
+							$("#card_oracle_text").text(card_oracle_text);
+
+							//show the image of the current card being searched
+							$('#card_img_front').attr('src', card_img_front);
+						});
+		        	}
+				});
+	    	}
+	    }, delay));
+	});
 });
