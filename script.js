28b4d236b1098d7adbd3a15a9c2ada677c6262cb
oaklandmurphy <aidan@niallmurphy.net>
2021-12-12 16:19:03 -0800

    Add files via upload
    updated search method to give continuous suggestions for cards, also added card-back placeholder when there are no matches

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
 var endpoint = "https://api.scryfall.com/"
-var named = "cards/named?fuzzy="
-var search = "cards/search?fuzzy="
 
+function display_default() {
+	// print the current guess for the card being searched
+	$("#card_title").text("No Card Searched");
 
-// activated when the serach bar is edited
+	// print the current guess for the oracle text of the card being searched
+	$("#card_oracle_text").text("No Card Searched");
+
+	//show the image of the current card being searched
+	$('#card_img_front').attr('src', "https://c1.scryfall.com/file/scryfall-card-backs/large/59/597b79b3-7d77-4261-871a-60dd17403388.jpg?1561757042");
+
+}
+
+// set all feilds to default values on page access
+display_default()
+
+// activated when the search bar is edited
 $(document).ready(function(){
     $("#card_name_search").on("input", function(){
 
         // store the current input in the bar
         var card_searched = $(this).val()
+        console.log(card_searched)
 
-        // try to access the scryfall
-        $.getJSON("https://api.scryfall.com/cards/named?fuzzy=" + card_searched, function(data){
-			// store relevant card data
-			var card_title = data.name
-			var card_oracle_text = data.oracle_text
-			var card_img_front = data.image_uris.large
+        // if the serch is too short to match any cards show the default
+        if(card_searched.length < 2) {	
 
+			display_default()
 
-			// print the current guess for the card being searched
-			$("#card_title").text(card_title);
+		//otherwise search
+        }else {
 
-			// print the current guess for the oracle text of the card being searched
-			$("#card_oracle_text").text(card_oracle_text);
+	        // try to access the scryfall api for matching cards
+	        $.getJSON("https://api.scryfall.com/cards/autocomplete?q=" + card_searched, function(choices){
+				
+				// if there are no matches show the default
+	        	if(choices.data.length == 0) {
+	        		display_default()
 
-			//show the image of the current card being searched
-			$('#card_img_front').attr('src', card_img_front);
+	        	// otherwise grab the best match's data
+	        	}else {
 
-		});
+		        	var best_match = choices.data[0]
+
+		        	console.log(choices)
+
+		        	$.getJSON("https://api.scryfall.com/cards/named?fuzzy=" + best_match, function(data){
+						// store relevant card data
+						var card_title = data.name
+						var card_oracle_text = data.oracle_text
+						var card_img_front = data.image_uris.large
+
+						// print the current guess for the card being searched
+						$("#card_title").text(card_title);
+
+						// print the current guess for the oracle text of the card being searched
+						$("#card_oracle_text").text(card_oracle_text);
+
+						//show the image of the current card being searched
+						$('#card_img_front').attr('src', card_img_front);
+					});
+	        	}
+			});
+    	}
     });
 });
